05334834a8b9d5a37e9b819294c63b075cb84a84
Jenni <silverrain64@gmail.com>
2017-02-27 03:12:45 -0800

    Fixed business-day calculation and updated CSS

--- DueWhen/index.js
+++ DueWhen/index.js
@@ -1,51 +1,55 @@
 $('document').ready(function() {
   "use strict";
 
-  var shortf = 'MMMM Do YYYY';
-  var longf = 'MMMM Do YYYY, h:mm:ss a';
-  var rightNow = moment().format(longf);
-  var endDate;
+	var shortf = 'MMMM Do YYYY';
+	var longf = 'MMMM Do YYYY, h:mm:ss a';
+	var rightNow = moment().format(longf);
+	var endDate;
 
-  $('.today').html(rightNow);
-  $('#rect2, #rect3, #rect4, #rect5, #rect6').hide();
-  $('#itemID').focus();
+	$('.today').html(rightNow);
+	$('#rect2, #rect3, #rect4, #rect5, #rect6').hide();
+	$('#itemID').focus();
 
-  function assembleDate() {
-    var mm = $('#month').val();
-    var dd = $('#day').val();
-    var yy = $('#year').val();
-    var hh = $('#dueByH').val();
-    if (hh < 12) {
-      hh = Number(hh) + 12;
-    }
-    if (hh.length < 2) {
-      hh = '0' + hh;
-    }
-    var mi = $('#dueByM').val();
-    if (dd.length < 2) {
-      dd = '0' + dd;
-    }
-    if (mm.length < 2) {
-      mm = '0' + mm;
-    }
-    var dateStr = yy + mm + dd;
-    var timeStr = ' ' + hh + ':' + mi + ':00';
-    var endStr = dateStr + timeStr;
-    console.log('assembled date: ' + endStr);
-    return endStr;
-  }
 
+////Assembles the date from #rect2 inputs
+	function assembleDate() {
+		var mm = $('#month').val();
+		var dd = $('#day').val();
+		var yy = $('#year').val();
+		var hh = $('#dueByH').val();
+		if (hh < 12) {
+			hh = Number(hh) + 12;
+		}
+		if (hh.length < 2) {
+		    hh = '0' + hh;
+		}
+		var mi = $('#dueByM').val();
+		if (dd.length < 2) {
+		    dd = '0' + dd;
+		}
+		if (mm.length < 2) {
+		    mm = '0' + mm;
+		}
+		var dateStr = yy + mm + dd;
+		var timeStr = ' ' + hh + ':' + mi + ':00';
+		var endStr = dateStr + timeStr;
+
+		return endStr;
+	}
+
+
+////Finds the difference between dates, converts to a valid time, and prints it in English.
+////Probably doing this the hard way.
   function calcTimeLeft(date) {
     var nowArr = moment().toArray();
-    console.log(nowArr);
     var thenArr = moment(date).toArray();
-    console.log(thenArr);
     var timeArr = [];
     for (var i = 0; i < 5; i++) {
       timeArr.push(thenArr[i] - nowArr[i]);
-      console.log('raw timeArr: ' + timeArr);
+      //console.log('raw timeArr: ' + timeArr);
     }
     
+    ////Converting minutes to hours, hours to days, etc.
     if (timeArr[4] < 0) {
       timeArr[4] += 60;
       timeArr[3]--;
@@ -75,53 +79,54 @@ $('document').ready(function() {
       timeArr[4] -= 60;
       timeArr[3]++;
     }
-    console.log('adjusted timeArr: ' + timeArr);
-    if (timeArr[1] < 0) {
-      $('#dueResults').html("Sorry, you' missed the deadline!");
+
+    ////Displaying time left
+    if (timeArr[0] < 0) {
+      $('#dueResults').html("Sorry, you missed the deadline!");
       return;
     } else if (timeArr[1] > 0) {
-      $('#limit').html(' in ' + timeArr[1] + ' month(s) and ' + timeArr[2] + ' days');    
+      	$('#limit').html(' in ' + timeArr[1] + ' month(s) and ' + timeArr[2] + ' days');    
     } else if (timeArr[2] >= 3) {
-      $('#limit').html(' in ' + timeArr[2] + ' days');
+      	$('#limit').html(' in ' + timeArr[2] + ' days');
     } else {
-      $('#limit').html(timeArr[2] + ' days, ' + timeArr[3] + ' hours, and ' + timeArr[4] + ' minutes ');
+      	$('#limit').html(timeArr[2] + ' days, ' + timeArr[3] + ' hours, and ' + timeArr[4] + ' minutes ');
     }
   }
 
-  function numCheck(n) {
-    var reg = /\D/gi;
-    var x = n.match(reg);
-    if (x !== null && x !== NaN) {
-      alert('Please enter a valid number');
-      return false;
-    } else {
-      return true;
-    }
-  }
+	////Checks whether the input is a valid number
+	function numCheck(n) {
+	    var reg = /\D/gi;
+	    var x = n.match(reg);
+	    if (x !== null && x !== NaN) {
+	      alert('Please enter a valid number');
+	      return false;
+	    } else {
+	      return true;
+	    }
+	  }
 
-  function calcBizDays(x, y) {
-    var a = 0;
-    var arr = [];
-    for (var i = 0; i < x; i++) {
-      var dayCheck = moment().add(i, 'day').day();
-      arr.push(dayCheck);
-      if (dayCheck === 0 || dayCheck === 6) {
-        a++;
-      }
-    }
-    var b = 0;
-    var barr = [];
-    for (var j = x; j <= y; j++) {
-      var dayCheck = moment().add(j, 'day').day();
-      barr.push(dayCheck);
-      if (dayCheck === 0 || dayCheck === 6) {
-        b++;
-      }
-    }
-    var z = [x += a, y += (b + a)];
-    return z;
-  }
+	////Calculates how many non-business days are within the date range
+	function calcBizDays(x, y) {
+	    var a = 0;
+	    for (var i = 0; i < x; i++) {
+	      var dayCheck = moment().add(i, 'day').day();
+	      if (dayCheck === 0 || dayCheck === 6) {
+	        a++;
+	      }
+   		}
+	    var b = 0;
+	    for (var j = x; j <= y; j++) {
+	      var dayCheck = moment().add(j, 'day').day();
+	      if (dayCheck === 0 || dayCheck === 6) {
+	        b++;
+	      }
+	    }
+	    var c = [x += a, y += (b + a)];
+	    return c;
+  	}
 
+
+////Button controls
   $('#step1').on('click', function() {
     var issue = $('#itemID').val();
     $('.results').html(issue);
@@ -130,13 +135,6 @@ $('document').ready(function() {
           document.body.scrollTop = $(this).offset().top;
         });
   });
-
-  function twoDigits(n) {
-    if (n.length === 1) {
-      n = '0' + String(n);
-    }
-    return n;
-  }
   
   $('#step2').on('click', function() {
     var m = $('#month').val();
@@ -191,8 +189,21 @@ $('document').ready(function() {
         var bizDays = calcBizDays(low, high);
         low = bizDays[0];
         high = bizDays[1];
-        lowEst = moment().add(low, 'day').format(shortf);
-        highEst = moment().add(high, 'day').format(shortf);
+
+        lowEst = moment().add(low, 'day');
+        while(lowEst.day() === 6 || lowEst.day() === 0){
+        	lowEst = lowEst.add(1, 'day');
+   		}
+   		lowEst = lowEst.format(shortf);
+
+        highEst = moment().add(high, 'day');
+        while(highEst.day() === 6 || highEst.day() === 0){
+        highEst = highEst.add(1, 'day');
+    	}
+		highEst = highEst.format(shortf);
+        
+        
+
         $('#rect4, #rect3, #rect2, #rect1').hide();
         $('#rect5').show();
         $('#low').html(lowEst);
@@ -226,4 +237,8 @@ $('document').ready(function() {
     window.open(tweety, '_blank');
   });
 
+	$('#month').val(moment().month()+1);
+	$('#day').val(moment().date());
+	$('#year').val(moment().year());
+	console.log(moment().day());
 });
