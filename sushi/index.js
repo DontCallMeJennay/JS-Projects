fe0cc7a1ede5a127f0b12f9d1728be35c4d25a00
Jenni <silverrain64@gmail.com>
2017-06-24 14:02:37 -0700

    Refactoring and tracking down bugs
    FFS, I wish the service didn't obfuscate its variables. I'm having to
    test line-by-line to figure out what the error messages are referring
    to.

--- sushi/index.js
+++ sushi/index.js
@@ -1,158 +1,226 @@
-function heyWaitAMinute(){
+/*
+REFACTORING because this code sucks.
+What are we doing, exactly?
 
-var markArr = [];
+0.  Get API keys
 
-function getLocation() {  
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 60000
-    };
+1.  Geolocation enabled ? Get location and draw map : Display error, slideDown manual search;
+    return coordinates;
 
-    function success(pos) {
-      var x = pos.coords.latitude;
-      var y = pos.coords.longitude;
-      obj = {
-        lat: x, 
-        lng: y
-      };
-      bounds = new google.maps.LatLngBounds();
-      initMap(obj, bounds);
-      console.log(x + ", " + y);
-      getSushi(obj);
+2.  Query API for local restaurants
+    return data set;
+
+3.  For each data point
+      Construct list item;
+      Make map marker;
+    Display updated map
+
+4.  For each list item,
+      Add sort function;
+      Onclick = get directions;
+*/
+
+
+function initMap() {
+    var myKey;
+    var bounds;
+    var coords;
+    var markArr = [];
+    $.getJSON("test.json", function(data) {
+        myKey = JSON.stringify(data.snippet1);
+    });
+
+    function getLocation() {
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 60000
+        };
+
+        function success(pos) {
+            var x = pos.coords.latitude;
+            var y = pos.coords.longitude;
+            obj = {
+                lat: x,
+                lng: y
+            };
+            bounds = new google.maps.LatLngBounds();
+            coords = obj;
+            drawMap(coords);
+        }
+
+        function error(err) {
+            console.error(err);
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error, options);
+        } else {
+            $("#msg").html("Geolocation unavailable. Manual search required.");
+            slideItems(formFields);
+        }
     }
 
-    function error(err) {
-       console.log(err);
+    coords = getLocation();
+
+    function addMarker(pos) {
+        var mark = new google.maps.Marker({
+            map: myMap,
+            position: pos.geometry.location,
+            title: pos.name + '\n' + pos.vicinity
+        });
+        bounds.extend(pos.geometry.location);
+        markArr.push(mark);
     }
 
-   if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  } else {
-    $("#map").html("Geolocation disabled. Manual search required.")
-  }
+    function drawMap(coords) {
+
+        if (coords) {
+            var mapDiv = document.getElementById('map');
+            myMap = new google.maps.Map(mapDiv, {
+                center: coords,
+                zoom: 14
+            });
+            var marker = new google.maps.Marker({
+                position: coords,
+                map: myMap,
+                title: 'You Are Here'
+            });
+            myMap.setCenter(coords);
+            bounds.extend(coords);
+            console.log("Coords: " + JSON.stringify(coords));
+            console.log("myMap: " + myMap);
+            console.log("bounds: " + JSON.stringify(bounds));
+            getSushi(coords, myMap);
+        } else {
+            $(mapDiv).html('Enter your address or zip code.');
+            slideItems(formFields);
+            console.log("Coords: " + coords);
+            console.log("myMap: " + myMap);
+            console.log("bounds: " + bounds);
+            console.log('Map draw failed');
+        }
+    }
+
+    function getSushi(coords, map) {
+        var service = new google.maps.places.PlacesService(map);
+        var request = {
+            location: coords,
+            radius: '2000',
+            keyword: 'sushi'
+        };
+        service.nearbySearch(request, sortData);
+    }
+
+    function sortData(response, status) {
+        console.log("sortData() called");
+        console.log(response);
+        for (var i = 0; i < response.length; i++) {
+            var x = response[i];
+            if (x.types !== "gas station") {
+                addMarker(response[i]);
+                $('.target').append('<div class="result" id="listing' + i + '"><a href="#">' + response[i].name + '</a><br>');
+                if (x.vicinity) {
+                    $('#listing' + i).append('<span class="addy">' + response[i].vicinity + '</span><br>');
+                }
+                if (x.rating) {
+                    $('#listing' + i).append('Rating: <span>' + response[i].rating + '</span><br>');
+                }
+                if (x.price_level) {
+                    $('#listing' + i).append('Price: <span>' + response[i].price_level + '</span><br>');
+                }
+                if (x.opening_hours) {
+                    var isOpen = response[i].opening_hours.open_now;
+                    if (isOpen) {
+                        $('#listing' + i).append('Open now? <span> Yes </span><br>');
+                    } else {
+                        $('#listing' + i).append('Open now? <span> No </span><br>');
+                    }
+                }
+                $('#listing' + i).append('</div>');
+            }
+        }
+        google.maps.event.trigger(myMap, 'resize');
+        myMap.fitBounds(bounds);
+
+    }
+    //getDirections();
 }
 
-function initMap(obj, bounds) {
-if (navigator.geolocation) {
-    var mapDiv = document.getElementById('map');
-    myMap = new google.maps.Map(mapDiv, {
-      center: obj,
-      zoom: 14
-      });   
-    var marker = new google.maps.Marker({
-       position: obj,
-       map: myMap,
-       title: 'You Are Here'
-       });    
-    myMap.setCenter(obj);
-    bounds.extend(obj);
-    } else {
-    $('#mapDiv').html('Enter your address or zip code');
-    console.log('Geolocation failed');
-    }
-  }
-
-getLocation();
-
-  $('#search').on('click', function() {
-    var address, city, zipcode = "";
+$('#findRestaurants').on('click', function() {
     var address = $('#address').val() + ' ';
     var city = $('#city').val();
     var zipcode = $('#zip').val();
     if (true) {
-      var str = address + city + zipcode;
-      str = str.replace(/ /gm, '+');
-      $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + str + '&key=AIzaSyAqVmW0IH-42cxUSLIvynm8tx-N3VVD50Q', function (data) {
-          console.log(data);
-          var data = {
-          lat: data.results[0].geometry.location.lat,
-          lng: data.results[0].geometry.location.lng
-          };
-          if (markArr[0]) {
-            for (i=0; i < markArr.length; i++) {
-              markArr[i].setMap(null);
+        var str = (address + city + zipcode).replace(/ /gm, '+');
+        console.log(str);
+        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + str + '&key=' + myKey, function(data) {
+            var newCoords = {
+                lat: data.response[0].geometry.location.lat,
+                lng: data.response[0].geometry.location.lng
+            };
+            console.log("Search coords: " + newCoords);
+            if (markArr[0]) {
+                for (i = 0; i < markArr.length; i++) {
+                    markArr[i].setMap(null);
+                }
+                markArr = [];
+                $('.target').html("");
             }
-            markArr = [];
-            $('.target').html("");
-            }
-          myMap.setCenter(data);
-          getSushi(data);
+            myMap.setCenter(newCoords);
+            //getSushi(newCoords, myMap);
         });
     }
-  });
-  
-  function addMarker(pos) {
-    var mark = new google.maps.Marker({
-      map: myMap,
-      position: pos.geometry.location,
-      title: pos.name + '\n' + pos.vicinity
-    });
-    bounds.extend(pos.geometry.location);
-
-    markArr.push(mark);
-  }  
-  
-  function getSushi(data) {
-    var service = new google.maps.places.PlacesService(myMap);
-    var request = {
-      location: data,
-      radius: '2000',
-      keyword: 'sushi'
-  };
-    service.nearbySearch(request, callback);
-    }
-    
-    
-  function callback(results, status) {
-    for (var i=0; i < results.length; i++) {
-      var x = results[i];
-      if (x.types !== "gas station") {
-        addMarker(results[i]);
-        $('.target').append('<div class="result" id="listing'+i+'"><a href="#">' + results[i].name + '</a><br>');
-      if (x.vicinity) {
-        $('#listing'+i).append('<span class="addy">' + results[i].vicinity + '</span><br>');
-        }
-      if (x.rating) {
-        $('#listing'+i).append('Rating: <span>' + results[i].rating + '</span><br>');
-        }
-      if (x.price_level) {
-        $('#listing'+i).append('Price: <span>' + results[i].price_level + '</span><br>');
-        }
-      if (x.opening_hours) {      
-        var isOpen = results[i].opening_hours.open_now;
-        if (isOpen) {
-          $('#listing'+i).append('Open now? <span> Yes </span><br>');
-        } else {
-          $('#listing'+i).append('Open now? <span> No </span><br>');
-          }
-                }
-                $('#listing'+i).append('</div>');
-    }
-    }
-    google.maps.event.trigger(myMap, 'resize');
-    myMap.fitBounds(bounds);
-    }
-}
+});
 
 function slideItems(obj) {
     $(obj).toggleClass("show");
-    if ($(obj).hasClass("show")) {
-        $(obj).slideDown(300);
+    if ((obj).hasClass("show")) {
+        (obj).slideDown(300);
+
     } else {
         $(obj).slideUp(300);
     }
 }
 
 
-
 var formFields = $("form > input, form > button");
 $(formFields).hide();
 
 $("#searchForm > legend").on("click", function() {
-  slideItems(formFields);
+    slideItems(formFields);
 });
 
 $("#results").on("click", function() {
-  slideItems($(".target"));
+    slideItems($(".target"));
 });
+
+
+/* ------
+        for (var i = 0; i < response.length; i++) {
+            var x = response[i];
+            if (x.types !== "gas station") {
+                //addMarker(response[i]);
+                $('.target').append('<div class="result" id="listing' + i + '"><a href="#">' + response[i].name + '</a><br>');
+                if (x.vicinity) {
+                    $('#listing' + i).append('<span class="addy">' + response[i].vicinity + '</span><br>');
+                }
+                if (x.rating) {
+                    $('#listing' + i).append('Rating: <span>' + response[i].rating + '</span><br>');
+                }
+                if (x.price_level) {
+                    $('#listing' + i).append('Price: <span>' + response[i].price_level + '</span><br>');
+                }
+                if (x.opening_hours) {
+                    var isOpen = response[i].opening_hours.open_now;
+                    if (isOpen) {
+                        $('#listing' + i).append('Open now? <span> Yes </span><br>');
+                    } else {
+                        $('#listing' + i).append('Open now? <span> No </span><br>');
+                    }
+                }
+                $('#listing' + i).append('</div>');
+            }
+        }
+
+------ */
