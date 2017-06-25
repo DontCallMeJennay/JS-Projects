0586a1d1de6c694ec9e9995af07d6d3fe1b49d62
Jenni <silverrain64@gmail.com>
2017-06-25 01:18:16 -0700

    Improved search results, UI effects, and struggled with marker creation

--- sushi/index.js
+++ sushi/index.js
@@ -1,30 +1,34 @@
 /*
-REFACTORING because this code sucks.
-What are we doing, exactly?
+DONE:
+Get API keys v/
+Geolocation enabled ? Get location and draw map : Display error, slideDown manual search; v/
+return coordinates; v/
+Query API for local restaurants v/
+return data set; v/
+For each data point
+  Construct list item; v/
+  Make map marker; v/
+  Display updated map v/
 
-0.  Get API keys
+TODO:
+1.  Make markers clickable
+2.  Make map zoom and center on clicked/touched marker
+3.  Make results more relevant; default sort by rating.
+4.  Let user choose how many results are displayed
+5.  Let user filter results by price, rating, and hours
+6.  Let user set range
 
-1.  Geolocation enabled ? Get location and draw map : Display error, slideDown manual search;
-    return coordinates;
-
-2.  Query API for local restaurants
-    return data set;
-
-3.  For each data point
-      Construct list item;
-      Make map marker;
-    Display updated map
-
-4.  For each list item,
-      Add sort function;
-      Onclick = get directions;
 */
-
 
 function initMap() {
     var bounds;
     var coords;
+    var myKey;
     var markArr = [];
+
+    $.getJSON("test.json", function(data) {
+      myKey = data.snippet1;
+    });
 
     function getLocation() {
         var options = {
@@ -59,14 +63,14 @@ function initMap() {
 
     coords = getLocation();
 
-    function addMarker(pos) {
-        var mark = new google.maps.Marker({
+    function createMarker(hit, i) {
+        let loc = hit.geometry.location;
+        return new google.maps.Marker({
+            clickable: true,
+            position: loc,
             map: myMap,
-            position: pos.geometry.location,
-            title: pos.name + '\n' + pos.vicinity
+            title: hit.name + '\n' + hit.vicinity
         });
-        bounds.extend(pos.geometry.location);
-        markArr.push(mark);
     }
 
     function drawMap(coords) {
@@ -77,22 +81,17 @@ function initMap() {
                 zoom: 14
             });
             var marker = new google.maps.Marker({
+                clickable: true,
                 position: coords,
                 map: myMap,
                 title: 'You Are Here'
             });
             myMap.setCenter(coords);
             bounds.extend(coords);
-            console.log("Coords: " + JSON.stringify(coords));
-            console.log("myMap: " + myMap);
-            console.log("bounds: " + JSON.stringify(bounds));
             getSushi(coords, myMap);
         } else {
             $(mapDiv).html('Enter your address or zip code.');
             slideItems(formFields);
-            console.log("Coords: " + coords);
-            console.log("myMap: " + myMap);
-            console.log("bounds: " + bounds);
             console.log('Map draw failed');
         }
     }
@@ -107,13 +106,28 @@ function initMap() {
         service.nearbySearch(request, sortData);
     }
 
+    function setClick(marker) {
+        $(marker).on("click", function() {
+            console.log("click");
+            myMap.panTo(marker.position);
+        });
+    }
+
     function sortData(response, status) {
         $(".target").html("");
+        dataObj = {};
+
+        var textBlock = "";
         for (var i = 0; i < response.length; i++) {
             var x = response[i];
-            if (x.types !== "gas station") {
-                addMarker(response[i]);
-                $('.target').append('<div class="result" id="listing' + i + '"><a href="#">' + response[i].name + '</a><br>');
+            //console.log(x.types);
+            if (x["types"].includes("restaurant")) {
+                
+                setClick(createMarker(response[i]));
+                bounds.extend(response[i].geometry.location);
+
+                $('.target').append('<div class="result" id="listing' + i + '"><a href="https://www.google.com/maps/dir/' + response[i].name + '">' + response[i].name + '</a><br>');
+
                 if (x.vicinity) {
                     $('#listing' + i).append('<span class="addy">' + response[i].vicinity + '</span><br>');
                 }
@@ -133,62 +147,74 @@ function initMap() {
                 }
                 $('#listing' + i).append('</div>');
             }
+
         }
+
         google.maps.event.trigger(myMap, 'resize');
         myMap.fitBounds(bounds);
 
     }
     //getDirections();
-$('#findRestaurants').on('click', function(e) {
-    e.preventDefault();
-    bounds = ""; coords = "";
-    var address = $('#address').val() + ' ';
-    var city = $('#city').val();
-    var zipcode = $('#zip').val();
-    var str = (address + city + zipcode).replace(/ /gm, '+');
-    console.log(str);
-    $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + str, function(data) {
-      console.log(data);
-        var newCoords = {
-            lat: data.results[0].geometry.location.lat,
-            lng: data.results[0].geometry.location.lng
-        };
-        console.log("Search coords: " + newCoords);
-        if (markArr[0]) {
-            for (i = 0; i < markArr.length; i++) {
-                markArr[i].setMap(null);
+
+    $('#findRestaurants').on('click', function(e) {
+        e.preventDefault();
+        $(formFields).slideUp(300);
+        bounds = "";
+        coords = "";
+        var address = $('#address').val() + ' ';
+        var city = $('#city').val();
+        var zipcode = $('#zip').val();
+        var str = (address + city + zipcode).replace(/ /gm, '+');
+        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + str, function(data) {
+            var newCoords = {
+                lat: data.results[0].geometry.location.lat,
+                lng: data.results[0].geometry.location.lng
+            };
+            if (markArr[0]) {
+                for (i = 0; i < markArr.length; i++) {
+                    markArr[i].setMap(null);
+                }
+                markArr = [];
+                $('.target').html("");
             }
-            markArr = [];
-            $('.target').html("");
-        }
-        myMap.setCenter(newCoords);
-        console.log("setCenter to " + newCoords);
-        bounds = new google.maps.LatLngBounds(newCoords);
-        getSushi(newCoords, myMap);
+            myMap.setCenter(newCoords);
+            bounds = new google.maps.LatLngBounds(newCoords);
+            getSushi(newCoords, myMap);
+        });
     });
-});
 
-function slideItems(obj) {
-    $(obj).toggleClass("show");
-    if ((obj).hasClass("show")) {
-        (obj).slideDown(300);
+    function slideItems(obj) {
+        $(obj).toggleClass("show");
+        if ((obj).hasClass("show")) {
+            (obj).slideDown(300);
 
-    } else {
-        $(obj).slideUp(300);
+        } else {
+            $(obj).slideUp(300);
+        }
     }
-}
 
-var formFields = $("form > input, form > button");
-$(formFields).hide();
+    var formFields = $("form > input, form > button");
+    $(formFields).hide();
 
-$("#searchForm > legend").on("click", function() {
-    slideItems(formFields);
+    $("#searchForm > legend").on("click", function() {
+        slideItems(formFields);
+    });
+
+    $("#results").on("click", function() {
+        slideItems($(".target"));
+    });
+
+/*  Debugging function and variables for constructing driving direction GETs
+
+$("*").on('click', function() {
+  alert(this + "clicked");
 });
 
-$("#results").on("click", function() {
-    slideItems($(".target"));
-});
-
+  var directionsURL1 = "https://maps.googleapis.com/maps/api/directions/json?origin=";
+  var directionsURL2 = coords.lat + "," + coords.lng;
+  var destination = response[i].place_id;
+  var directionsURL3 = "&destination=" + response[i].geometry.location.lat + "," + response[i].geometry.location.lng;
+          
+*/
 
 }
-
