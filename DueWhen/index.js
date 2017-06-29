1b3538c94d860d8c78c1af7aec19561b4d456a5a
Jenni <silverrain64@gmail.com>
2017-06-29 01:42:10 -0700

    Applying principles from Clean Code.  Started with 220 lines of code, ended with 140. Niiice.

--- DueWhen/index.js
+++ DueWhen/index.js
@@ -1,223 +1,139 @@
 $('document').ready(function() {
-  "use strict";
 
-	var shortf = 'MMMM Do YYYY';
-	var longf = 'MMMM Do YYYY, h:mm:ss a';
-	var rightNow = moment().format(longf);
-	var endDate;
-
-	$('.today').html(rightNow);
-	$('#rect2, #rect3, #rect4, #rect5, #rect6').hide();
-
-////Assembles the date from #rect2 inputs
-	function assembleDate() {
-		var mm = $('#month').val();
-		var dd = $('#day').val();
-		var yy = $('#year').val();
-		var hh = $('#dueByH').val();
-		if (hh < 12) {
-			hh = Number(hh) + 12;
+	function checkInput(val) {
+		var reg = /\D/g;
+		if(val.match(reg) || val === "" || !val) {
+			alert(val + " is not a valid input.");
+			return false;
 		}
-		if (hh.length < 2) {
-		    hh = '0' + hh;
-		}
-		var mi = $('#dueByM').val();
-		if (dd.length < 2) {
-		    dd = '0' + dd;
-		}
-		if (mm.length < 2) {
-		    mm = '0' + mm;
-		}
-		var dateStr = yy + mm + dd;
-		var timeStr = ' ' + hh + ':' + mi + ':00';
-		var endStr = dateStr + timeStr;
-
-		return endStr;
+		return true;
 	}
 
-
-////Finds the difference between dates, converts to a valid time, and prints it in English.
-////Probably doing this the hard way.
-  function calcTimeLeft(date) {
-    var nowArr = moment().toArray();
-    var thenArr = moment(date).toArray();
-    var timeArr = [];
-    for (var i = 0; i < 5; i++) {
-      timeArr.push(thenArr[i] - nowArr[i]);
-      //console.log('raw timeArr: ' + timeArr);
-    }
-    
-    ////Converting minutes to hours, hours to days, etc.
-    if (timeArr[4] < 0) {
-      timeArr[4] += 60;
-      timeArr[3]--;
-    }
-    
-    if (timeArr[3] < 0) {
-      timeArr[3] += 24;
-      timeArr[2]--;
-    }
-
-    if (timeArr[2] < 0) {
-      timeArr[2] += 31;
-      timeArr[1]--;
-    }
-    
-    if (timeArr[2] > 30) {
-      timeArr[2] -= 31;
-      timeArr[1]++;
-    }
-      
-    if (timeArr[3] > 23) {
-      timeArr[3] -= 24;
-      timeArr[2]++;
-    }
-
-    if (timeArr[4] > 59) {
-      timeArr[4] -= 60;
-      timeArr[3]++;
-    }
-
-    ////Displaying time left
-    if (timeArr[0] < 0) {
-      $('#dueResults').html("Sorry, you missed the deadline!");
-      return;
-    } else if (timeArr[1] > 0) {
-      	$('#limit').html(' in ' + timeArr[1] + ' month(s) and ' + timeArr[2] + ' days');    
-    } else if (timeArr[2] >= 3) {
-      	$('#limit').html(' in ' + timeArr[2] + ' days');
-    } else {
-      	$('#limit').html(timeArr[2] + ' days, ' + timeArr[3] + ' hours, and ' + timeArr[4] + ' minutes ');
-    }
-  }
-
-	////Checks whether the input is a valid number
-	function numCheck(n) {
-	    var reg = /\D/gi;
-	    var x = n.match(reg);
-	    if (x !== null && x !== NaN) {
-	      alert('Please enter a valid number');
-	      return false;
-	    } else {
-	      return true;
-	    }
-	  }
-
-	////Calculates how many non-business days are within the date range
-	function calcBizDays(x, y) {
-	    var a = 0;
-	    for (var i = 0; i < x; i++) {
-	      var dayCheck = moment().add(i, 'day').day();
-	      if (dayCheck === 0 || dayCheck === 6) {
-	        a++;
-	      }
-   		}
-	    var b = 0;
-	    for (var j = x; j <= y; j++) {
-	      var dayCheck = moment().add(j, 'day').day();
-	      if (dayCheck === 0 || dayCheck === 6) {
-	        b++;
-	      }
-	    }
-	    var c = [x += a, y += (b + a)];
-	    return c;
-  	}
-
-
-////Button controls
-  $('#step1').on('click', function() {
-    var issue = $('#itemID').val();
-    $('.results').html(issue);
-    $('#rect1').slideUp(400);
-    $('#rect2').delay(500).slideDown();
-  });
-  
-  $('#step2').on('click', function() {
-    var m = $('#month').val();
-    var d = $('#day').val();
-    var y = $('#year').val();
-    if (m === '' || d === '' || y === '') {
-      alert('Please enter a complete date');
-    } else {
-    
-    if (numCheck(m) && numCheck(d) && numCheck(y)) {
-      if ($('#specific').is(':checked')) {
-        $('#rect2').slideUp(400);
-        $('#rect4').delay(500).slideDown();
-        
-      } else if ($('#range').is(':checked')) {
-        $('#rect2').slideUp(400);
-        $('#rect3').delay(500).slideDown();
-      } else {
-        alert('Please select an option.');
-      }
+    function calculateDueDate() {
+    	var y = $('#year').val();
+    	var m = $("#month").val()
+    	var d =  $('#day').val();
+        var dueDate = y + m + d;
+        	if(checkInput(y) && checkInput(m) && checkInput(d)) {
+        dueDate = moment(dueDate).format('MM DD YYYY');
+        console.log("dueDate: " + dueDate);
+        return dueDate;
     }
     }
-  });
 
-  $('#step3').on('click', function() {
-    var deadline = assembleDate();
-    if ($('#noTime').is(':checked')) {
-      $('.rectangle:not(#rect6)').slideUp(400);
-      $('#rect6').delay(500).slideDown();
-      $('#limit').html(calcTimeLeft(deadline));
-
-    } else if ($('#yesTime').is(':checked')) {
-      if (numCheck($('#dueByH').val()) && (numCheck($('#dueByM').val()))) {
-      $('.rectangle:not(#rect6)').slideUp(400);
-      $('#rect6').delay(500).slideDown();
-        $('#limit').html(calcTimeLeft(deadline));
-      }
+    function calculateDueTime() {
+        var dueTime = $('#dueByH').val() + $("#dueByM").val();
+        $('#PM').is(':checked') ? dueTime += "p" : dueTime += "a";
+        dueTime = moment(dueTime, "hmm A").format("HH:mm");
+        console.log("dueTime: " + dueTime);
+        return dueTime;
     }
-  });
 
-  $('#timeFrame').on('click', function() {
-    var low = Number($('#minDays').val());
-    var high = Number($('#maxDays').val());
-    if (numCheck(String(low)) && numCheck(String(high))) {
-      var lowEst = moment().add(low, 'days').format(shortf);
-      var highEst = moment().add(high, 'days').format(shortf);
-      if ($('#business').is(':checked')) {
-        var bizDays = calcBizDays(low, high);
-        low = bizDays[0];
-        high = bizDays[1];
+    function calculateTimeLeft(dueDate, dueTime) {
+        var dateTime = dueDate + " " + dueTime;
+        var difference = [
+            //Bugfix: there's a deprecation and/or formatting issue on the line just below.
+            moment(dateTime).month() - moment().month(),
+            moment(dateTime).date() - moment().date(),
+            moment(dateTime).hour() - moment().hour(),
+            moment(dateTime).minute() - moment().minute()
+        ];
+        return fixTimeCalc(difference);
+    }
 
-        lowEst = moment().add(low, 'day');
-        while(lowEst.day() === 6 || lowEst.day() === 0){
-        	lowEst = lowEst.add(1, 'day');
-   		}
-   		lowEst = lowEst.format(shortf);
+    function fixTimeCalc(arr) {
 
-        highEst = moment().add(high, 'day');
-        while(highEst.day() === 6 || highEst.day() === 0){
-        highEst = highEst.add(1, 'day');
-    	}
-		highEst = highEst.format(shortf);
-        
+        function rollUnder(arr, i, limit) {
+            if (arr[i] < 0) {
+                arr[i] += limit;
+                arr[i - 1]--;
+            }
+        }
+
+        function rollOver(arr, i, limit) {
+            if (arr[i] > limit) {
+                arr[i] -= limit + 1;
+                arr[i - 1]++;
+            }
+        }
+
+        rollUnder(arr, 3, 60);
+        rollUnder(arr, 2, 24);
+        rollUnder(arr, 1, 31);
+
+        rollOver(arr, 1, 30);
+        rollOver(arr, 2, 23);
+        rollOver(arr, 3, 59);
+
+        return arr;
+    }
+
+    function calcDateRange(min, max) {
+        min = moment().add(min, "days").format("MMMM Do YYYY");
+        max = moment().add(max, "days").format("MMMM Do YYYY");
+        $("#low").html(min);
+        $("#high").html(max);
+    }
+
+    function addBusinessDays(num) {
+        for (var i = 0; i < num + 2; i++) {
+            var check = moment().add(i, "day").day();
+            if (check === 0 || check === 6) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    $("input[type=submit]").click(function(e) { e.preventDefault(); });
+    $('.today').html(moment().format("MMMM Do YYYY @ hh:mm A"));
+    $(".rectangle:not(#rect1)").hide();
+
+    $("#step1").click(() => {
+        var issue = $('#itemID').val();
+        $('.results').html(issue);
+        $('#rect1').slideUp(400);
+        $('#rect2').delay(500).slideDown();
+    });
+
+    $('#step2').on('click', function(event) {
+        if ($('#specific').is(':checked')) {
+            $('#rect2').slideUp(400);
+            $('#rect4').delay(500).slideDown();
+        } else {
+            $('#rect2').slideUp(400);
+            $('#rect3').delay(500).slideDown();
+        }
+    });
+
+    $("#step3").on("click", function(event) {
+        var dueDate = calculateDueDate();
+        var dueTime = calculateDueTime();
+        var final = calculateTimeLeft(dueDate, dueTime);
+        if ($('#noTime').is(':checked')) {
+            $('.rectangle:not(#rect6)').slideUp(400);
+            $('#rect6').delay(500).slideDown();
+            $("#limit").html(final[0] + " months and " + final[1] + " days");
+        } else {
+            $('.rectangle:not(#rect6)').slideUp(400);
+            $('#rect6').delay(500).slideDown();
+            $("#limit").html(final[0] + " months, " + final[1] + " days, " + final[2] + " hours, and " + final[3] + " minutes");
+        }
+    });
+
+    $("#step4").on("click", function(event) {
+        var min = $("#minDays").val();
+        var max = $("#maxDays").val();
+        if ($('#calendar').is(':checked')) {
+            calcDateRange(min, max);
+        } else {
+            min = addBusinessDays(min);
+            max = addBusinessDays(max);
+            calcDateRange(min, max);
+        }
         $('.rectangle:not(#rect5)').slideUp(400);
         $('#rect5').delay(500).slideDown();
-        $('#low').html(lowEst);
-        $('#high').html(highEst);
+    });
 
-      } else {
-        $('.rectangle:not(#rect5)').slideUp(400);
-        $('#rect5').delay(500).slideDown();
-        $('#low').html(lowEst);
-        $('#high').html(highEst);
-      }
-    }
-  });
-
-  $('#reset1, #reset2').on('click', function() {
-    $('.today').html(rightNow);
-    $('#limit, #low, #high').html('');
-    $('.rectangle:not(#rect1)').slideUp(400);
-    $('#rect1').delay(500).slideDown();
-
-  });
-
-	$('#month').val(moment().month()+1);
-	$('#day').val(moment().date());
-	$('#year').val(moment().year());
-	console.log(moment().day());
+    $(".reset").click(() => { location.reload(); });
 });
